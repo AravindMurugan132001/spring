85c974f5873d37fa40cead0d1d1f9c985751cb3f
AravindMurugan2001 <avmr2001abi@gmail.com>
2022-08-22 17:47:42 +0530

    Spring ver-1

--- src/main/java/com/nseit/spring/QuickSort.java
+++ src/main/java/com/nseit/spring/QuickSort.java
@@ -1,7 +1,9 @@
 package com.nseit.spring;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 public class QuickSort implements SortAlg {
+    @Qualifier
     @Override
     public int[] sort(int[] numbers){
         return numbers;
